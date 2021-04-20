3073956ab1c8b673683b959ca23af28f4627add3
toasted-coder <christmanholmes21@gmail.com>
2021-04-20 09:48:32 -0700

    added some comments to logout.js, finished up the login.js file

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,10 +1,12 @@
 const logout = async () => {
+  // POST method to end "session"
   const response = await fetch("/api/users/logout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
+    // if statement redirecting user to login page after loggin out
     document.location.replace("/");
   } else {
     alert(response.statusText);

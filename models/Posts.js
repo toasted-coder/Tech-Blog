011475e620a801762a83cce989372ccf697847ce
toasted-coder <christmanholmes21@gmail.com>
2021-05-30 12:50:06 -0700

    completed the Posts.js in models section

---
+++ models/Posts.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Posts extends Model {}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Posts",
+  }
+);
+module.exports = Posts;
